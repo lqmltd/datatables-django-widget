6624a0de8d27a441ec57509a985203facc5abccc
Alex Nathanail <adnathanail@hotmail.co.uk>
2025-04-18 16:07:47 +0100

    Format code with Black

--- src/app/demo_app/forms.py
+++ src/app/demo_app/forms.py
@@ -3,11 +3,10 @@ from utils.fields import ArrayField2D
 
 from . import models
 
+
 class DemoModelForm(forms.ModelForm):
     demo_array_field = ArrayField2D(column_names=("A", "B", "C", "D", "E"))
 
     class Meta:
         model = models.DemoModel
-        fields = (
-            "demo_array_field",
-        )
+        fields = ("demo_array_field",)
